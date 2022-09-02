a69d2db0dc5b81dc59f7643272b4acab3f4d57da
carlton <carltonmbaja0@gmail.com>
2022-09-02 18:09:54 +0300

    Complete

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,34 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
+
+  get '/messages' do
+    # get all the messages from the database
+    messages = Message.all.order(:created_at)
+    # send them back as a JSON array
+    messages.to_json
+  end
+
+
+  post '/messages' do
+    # post all the messages from the database
+    messages = Message.create(body: params[:body],  username: params[:username])
+    # send them back as a JSON array
+    messages.to_json
+  end
   
+
+  patch '/messages/:id' do
+    # patch all the messages from the database
+    messages = Message.find(params[:id])
+    # send them back as a JSON array
+    messages.update(body: params[:body])
+    messages.to_json
+  end
+
+  delete "/messages/:id" do
+    messages = Message.find(params[:id])
+    messages.destroy
+    messages.to_json
+  end
 end
